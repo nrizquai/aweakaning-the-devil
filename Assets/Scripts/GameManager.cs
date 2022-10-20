7b6af8f0455c924acc056490158616719aeeeae3
Lexos2003 <lexos203@gmail.com>
2022-10-20 12:01:04 +0200

    mainMenu interface and create other scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance ;
+
+    public GameObject mainMenu;
+    public GameObject returnButton;
+    public GameObject startButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        returnButton.SetActive(true);
+        mainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(returnButton);
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(3);
+        returnButton.SetActive(true);
+        mainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(returnButton);
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene(4);
+        returnButton.SetActive(true);
+        mainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(returnButton);
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        returnButton.SetActive(false);
+        mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(startButton);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
